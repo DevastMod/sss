3b2347b5743ea242b0ddf90de63922acc01fcf97
DevastMod <102901194+DevastMod@users.noreply.github.com>
2022-08-19 22:00:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import asyncio
+import websockets
+
+async def Spam():
+    async with websockets.connect("wss://devast69.devast.io:433") as websocket:
+        await websocket.send("[30,"12345678901234567890","0",-1,0,"bit.ly/devastmod",0,null,0]")
+        # await websocket.recv()
+
+asyncio.run(hello())
